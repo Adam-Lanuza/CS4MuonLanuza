650de7d05953e72a1d2678da5988a007c7e4bbc3
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2022-09-12 08:31:54 +0800

    Update Q1Ex1.java

--- q1ex1/Q1Ex1.java
+++ q1ex1/Q1Ex1.java
@@ -1,35 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package q1ex1;
 
 public class Q1Ex1 {
-    public static void main(String[] args) {
-        // Re:Zero
-	String anime1Name = "Re: Zero";
-	int anime1Release = 2016;
-	int anime1Episodes = 50;
-        
-	// AoT
-	String anime2Name = "Attack on Titan";
-	int anime2Release = 2013;
-	int anime2Episodes = 87;
-        
-	// JJK
-	String anime3Name = "Jujutsu Kaisen";
-	int anime3Release = 2020;
-	int anime3Episodes = 24;
+	public static void main(String[] args) {
+		// Re:Zero
+		String anime1Name = "Re: Zero";
+		int anime1Release = 2016;
+		int anime1Episodes = 50;
+	
+		// AoT
+		String anime2Name = "Attack on Titan";
+		int anime2Release = 2013;
+		int anime2Episodes = 87;
 
-        // Comparisons
-        int totalEpisodes = anime1Episodes + anime2Episodes + anime3Episodes;
-	boolean ageCompare = anime3Release < anime1Release;
-	boolean lengthCompare = anime2Episodes > anime3Episodes;
+		// JJK
+		String anime3Name = "Jujutsu Kaisen";
+		int anime3Release = 2020;
+		int anime3Episodes = 24;
 
-        System.out.println("How many episodes are there in total? " + totalEpisodes);
-        System.out.println("Is JJK older than Re:Zero? " + ageCompare);
-	System.out.println("Is AoT longer than JJK? " + lengthCompare);
-    }
-    
+		// Comparisons
+		int totalEpisodes = anime1Episodes + anime2Episodes + anime3Episodes;
+		boolean ageCompare = anime3Release < anime1Release;
+		boolean lengthCompare = anime2Episodes > anime3Episodes;
+
+		System.out.println("How many episodes are there in total? " + totalEpisodes);
+		System.out.println("Is JJK older than Re:Zero? " + ageCompare);
+		System.out.println("Is AoT longer than JJK? " + lengthCompare);
+	}
 }
