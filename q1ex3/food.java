3e0e2055545e497b991b36d8f7030d127d327f2d
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2022-09-19 09:03:21 +0800

    Update food.java

--- q1ex3/food.java
+++ q1ex3/food.java
@@ -1,19 +1,12 @@
 package q1ex3;
 
 public class food {
-    String name, taste;
-    double rating;
-    
-    public food(String name, String taste) {
-        this.name = name;
-        this.taste = taste;
-        this.rating = 0.00;
-    }
-    
-    public void displayAll() {
-        System.out.print(name + ",");
-        System.out.print(taste + ",");
-        System.out.print(rating);
-        System.out.print("\n\n");
-    }
+	String name, taste;
+	double rating;
+	
+	public food(String name, String taste) {
+		this.name = name;
+		this.taste = taste;
+		this.rating = 0.00;
+	}
 }
