ec7236ca67fef0e96a77e8058f8af7a837d66e39
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2022-09-19 09:06:12 +0800

    Update song.java

--- q1ex3/song.java
+++ q1ex3/song.java
@@ -1,19 +1,12 @@
 package q1ex3;
 
 public class song {
-    String title, artist;
-    int duration;
-    
-    public song(String title, String artist, int duration) {
-        this.title = title;
-        this.artist = artist;
-        this.duration = duration;
-    }
-    
-    public void displayAll() {
-        System.out.print(title + ",");
-        System.out.print(artist + ",");
-        System.out.print(duration);
-        System.out.print("\n\n");
-    }
+	String title, artist;
+	int duration;
+
+	public song(String title, String artist, int duration) {
+		this.title = title;
+		this.artist = artist;
+		this.duration = duration;
+	}
 }
